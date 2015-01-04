d41579226acc10ba7c56b2a2fbea33fb60f30048
pdostal <git@pdostal.cz>
2015-01-05 00:43:09 +0100

    Fix run => execute ... bundle namespace

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ namespace :bundle do
 
   desc "run bundle install and ensure all gem requirements are met"
   task :install do
-    run "cd #{current_path} && bundle install  --without=test --no-update-sources"
+    execute "cd #{current_path} && bundle install  --without=test --no-update-sources"
   end
 
 end
