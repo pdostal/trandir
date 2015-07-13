8a87ae5e8344835b4c0d1f1331b0b940bb4aecb9
pdostal <git@pdostal.cz>
2015-07-13 17:09:03 +0200

    Add jekyll task to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,15 @@ set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 
 namespace :deploy do
-  after :publishing, :uptime do
+  before :publishing, :build_site do
+    on roles(:app) do
+      within release_path do
+        execute :bundle,  "exec jekyll build"
+      end
+    end
+  end
+
+  after :build_site, :uptime do
     on roles(:web), in: :groups do
       uptime = capture(:uptime)
       "#{host.hostname} reports: #{uptime}"
